b28ece7e87e775d3ffa6156bbe4e49060c6f20c1
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-29 09:51:52 +0600

    area chart add

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart } from 'recharts';
+import { Area, AreaChart, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MyLineChart = () => {
     const data = [
@@ -48,9 +48,18 @@ const MyLineChart = () => {
       ];
       
     return (
-        <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      </LineChart>
+    //     <LineChart width={400} height={400} data={data}>
+    //     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    //     <XAxis dataKey={'name'}></XAxis>
+    //     <YAxis dataKey={'pv'}></YAxis>
+    //   </LineChart>
+      <AreaChart width={1200} height={400} data={data}>
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8"></Area>
+      <Area type="monotone" dataKey="pv" stroke="#8884d8" fill="#8886fr" ></Area>
+      <Tooltip/>
+      <XAxis dataKey={'name'}></XAxis>
+      <YAxis dataKey={'pv'}></YAxis>
+      </AreaChart>
     );
 };
 
